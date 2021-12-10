428b4a207e49a159bb71a877d8ef92b60ccde2f5
squiddaodev <squiddaodev@oprotonmail.com>
2021-12-10 16:38:19 +0800

    first commit

---
+++ abi/treasury.js
@@ -0,0 +1,4 @@
+module.exports = [
+  "function deposit(uint _amount, address _token, uint _profit) external returns (uint)",
+  "function toggle(uint8 _managing, address _address, address _calculator) external returns (bool)",
+];
