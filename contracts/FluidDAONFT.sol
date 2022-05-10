94b135d9c5d7998891bd1207d0a6a98dd608a176
Carter Carlson <cartercarlson@protonmail.com>
2022-05-10 12:42:04 -0700

    feat: royalty

--- contracts/FluidDAONFT.sol
+++ contracts/FluidDAONFT.sol
@@ -1,42 +1,51 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import "@openzeppelin/contracts/utils/Strings.sol";
+import {ERC721Enumerable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import {ERC721Royalty} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Royalty.sol";
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
 
-contract FluidDAONFT is ERC721Enumerable, Ownable, ReentrancyGuard {
+contract FluidDAONFT is ERC721, ERC721Enumerable, ERC721Royalty, Ownable, ReentrancyGuard {
+
+    event Minted(uint256 indexed tokenId, address receiver);
+    event Burned(uint256 indexed tokenId);
+
     using Strings for uint256;
     address public auctionHouse;
-    uint256 currentTokenId;
+    address public royaltyWallet;
+    uint256 public _totalSupply;
     string private _baseURIExtended;
     mapping(uint256 => string) _tokenURIs;
 
-    constructor() ERC721("FluidDAONFT", "FLDN") {}
+    constructor() ERC721("FluidDAONFT", "FLDN") {
+        _setDefaultRoyalty(royaltyWallet, 1000); // TODO: validate this is 10%
+    }
 
     modifier onlyAuctionHouse() {
         require(msg.sender == auctionHouse, "caller is not the auctionHouse");
         _;
     }
 
-    event Minted(uint256 indexed tokenId, address receiver);
-    event Burned(uint256 indexed tokenId);
-
     function setAuctionHouse(address _auctionHouse) external onlyOwner {
         auctionHouse = _auctionHouse;
+    }
+
+    function setRoyaltyWallet(address _royaltyWallet) external onlyOwner {
+        royaltyWallet = _royaltyWallet;
     }
 
     function mint(address receiver)
         external
         onlyAuctionHouse
         nonReentrant
-        returns (uint256 mintedTokenId)
+        returns (uint256)
     {
-        mintedTokenId = currentTokenId;
-        currentTokenId += 1;
-        _mint(receiver, mintedTokenId);
-        emit Minted(mintedTokenId, receiver);
+        _safeMint(receiver, ++_totalSupply, "");
+        emit Minted(_totalSupply, receiver);
+        return _totalSupply;
     }
 
     function burn(uint256 tokenId) external onlyAuctionHouse nonReentrant {
@@ -81,5 +90,26 @@ contract FluidDAONFT is ERC721Enumerable, Ownable, ReentrancyGuard {
         require(bytes(base).length != 0, "baseURI not set");
         // If there is a baseURI but no tokenURI, concatenate the tokenID to the baseURI.
         return string(abi.encodePacked(base, tokenId.toString()));
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable, ERC721Royalty)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721Royalty) {
+        super._burn(tokenId);
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal override (ERC721, ERC721Enumerable) {
+        super._beforeTokenTransfer(from, to, tokenId);
     }
 }
