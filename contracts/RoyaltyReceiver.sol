899c5dc7f125f791e1917076060ecb0ddad86c77
Carter Carlson <carter@metokens.com>
2022-05-16 06:00:20 -0700

    refactor: correct variable names

--- contracts/RoyaltyReceiver.sol
+++ contracts/RoyaltyReceiver.sol
@@ -5,25 +5,19 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract RoyaltyReceiver is Ownable {
-    address public recipient1;
-    address public recipient2;
+    address public stakingPool;
+    address public dao = 0xB17ca1BC1e9a00850B0b2436e41A055403512387;
     
-    function setRecipient1(address _recipient) external onlyOwner {
-        require(recipient1 == address(0), "already set");
-        recipient1 = _recipient;
-    }
-
-
-    function setRecipient2(address _recipient) external onlyOwner {
-        require(recipient2 == address(0), "already set");
-        recipient2 = _recipient;
+    function setStakingPool(address _recipient) external onlyOwner {
+        require(stakingPool == address(0), "already set");
+        stakingPool = _recipient;
     }
 
     function claimRoyalties(address token) external {
         // divide rewards by two - distribute 
         uint256 balance = IERC20(token).balanceOf(address(this));
         uint256 half = balance/2;
-        IERC20(token).transfer(recipient1, half);
-        IERC20(token).transfer(recipient2, half);
+        IERC20(token).transfer(stakingPool, half);
+        IERC20(token).transfer(dao, half);
     }
 }
